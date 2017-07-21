9239665e34c3d603cc5c28d7ae9cc8a2e3f9e5a9
zachnewburgh <zach.newburgh@gmail.com>
2017-07-21 08:00:44 -0400

    fix(navigation): Move navigation to AppComponent and remove NavigationComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  displayNav: boolean;
+
+  constructor() { }
+
+  toggleNav() {
+    this.displayNav = !this.displayNav;
+  }
 }
