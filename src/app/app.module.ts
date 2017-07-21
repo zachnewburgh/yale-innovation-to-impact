c1e62094c9497b98f300946e65656ae683b05962
zachnewburgh <zach.newburgh@gmail.com>
2017-07-21 08:03:02 -0400

    fix(navigation): Remove NavigationComponent from AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { SplashComponent } from './splash/splash.component';
 import { AboutComponent } from './about/about.component';
 import { TeamComponent } from './team/team.component';
@@ -11,7 +10,6 @@ import { ApplyComponent } from './apply/apply.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
     SplashComponent,
     AboutComponent,
     TeamComponent,
