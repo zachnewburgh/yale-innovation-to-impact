ca0e2220a4c82df0e4ec906d33cc1b2c57d643a0
zachnewburgh <zach.newburgh@gmail.com>
2017-08-16 23:03:47 -0400

    refactor(team): Migrate team to use CSS Grid and add JR and RH

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
+  teamMembers: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.teamMembers = {
+      sethFeuerstein: 'http://psychiatry.yale.edu/people/seth_feuerstein.profile',
+      patriciaSimon: 'https://medicine.yale.edu/psychiatry/people/p_simon.profile',
+      erikaSmith: 'http://ocr.yale.edu/about-ocr/our-staff/erika-smith',
+      janieMerkel: 'http://ycmd.yale.edu/people/janie-merkel',
+      rickHunt: 'http://yei.yale.edu/richard-hunt',
+      joanneRichardson: 'http://people.yale.edu/search/joanne_richardson.profile'
+    }
+  }
+
+  openPage(name: string) {
+    const url = this.teamMembers[name];
+    return url ? window.open(url) : '';
   }
 
 }
