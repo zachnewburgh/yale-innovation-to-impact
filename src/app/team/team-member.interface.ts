4d3d9b93274d412e9617d055244c8eee3a738655
zachnewburgh <zach.newburgh@gmail.com>
2017-08-17 21:21:45 -0400

    refactor(team): Abstract team members and their related attributes into 'teamMembers' and provide typechecking with ITeamMember

---
+++ src/app/team/team-member.interface.ts
@@ -0,0 +1,7 @@
+export interface ITeamMember {
+    id: string;
+    imgPath: string;
+    name: string;
+    role: string;
+    url: string;
+}
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITeamMember } from './team-member.interface';
 
 @Component({
   selector: 'app-team',
@@ -7,24 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  teamMembers: any;
+  teamMembers: ITeamMember[];
 
-  constructor() { }
-
-  ngOnInit() {
-    this.teamMembers = {
-      sethFeuerstein: 'http://psychiatry.yale.edu/people/seth_feuerstein.profile',
-      patriciaSimon: 'https://medicine.yale.edu/psychiatry/people/p_simon.profile',
-      erikaSmith: 'http://ocr.yale.edu/about-ocr/our-staff/erika-smith',
-      janieMerkel: 'http://ycmd.yale.edu/people/janie-merkel',
-      rickHunt: 'http://yei.yale.edu/richard-hunt',
-      joanneRichardson: 'http://people.yale.edu/search/joanne_richardson.profile'
-    }
+  constructor() {
+    this.teamMembers = [
+      {
+        id: 'seth',
+        imgPath: '../../assets/sf-sm.jpg',
+        name: 'Seth Feuerstein, MD, JD',
+        role: 'Program Director (PI)',
+        url: 'http://psychiatry.yale.edu/people/seth_feuerstein.profile'
+      },
+      {
+        id: 'patricia',
+        imgPath: '../../assets/ps-sm.jpg',
+        name: 'Patricia Simon, PhD',
+        role: 'Program Director',
+        url: 'https://medicine.yale.edu/psychiatry/people/p_simon.profile'
+      },
+      {
+        id: 'erika',
+        imgPath: '../../assets/es-bg.jpg',
+        name: 'Erika Smith, MBA',
+        role: 'Advisor',
+        url: 'http://ocr.yale.edu/about-ocr/our-staff/erika-smith'
+      },
+      {
+        id: 'janie',
+        imgPath: '../../assets/jm-sm.jpg',
+        name: 'Janie Merkel, PhD',
+        role: 'Advisor',
+        url: 'http://ycmd.yale.edu/people/janie-merkel'
+      },
+      {
+        id: 'rick',
+        imgPath: '../../assets/rh-bg.jpg',
+        name: 'Rick Hunt, MBA',
+        role: 'Faculty/Mentor Lead',
+        url: 'http://yei.yale.edu/richard-hunt'
+      },
+      {
+        id: 'joanne',
+        imgPath: '../../assets/jr-sm.jpg',
+        name: 'Joanne Richardson, BS',
+        role: 'Program Coordinator',
+        url: 'http://people.yale.edu/search/joanne_richardson.profile'
+      }
+    ]
   }
 
-  openPage(name: string) {
-    const url = this.teamMembers[name];
-    return url ? window.open(url) : '';
+  ngOnInit() {}
+
+  openPage(url: string): void {
+    window.open(url);
   }
 
 }
