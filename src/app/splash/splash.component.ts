9997e8e26ec16cb0dd4a70907c1a1e4a64252807
zachnewburgh <zach.newburgh@gmail.com>
2017-11-11 09:51:55 -0500

    feat(splash): Add date and dueDate

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -7,14 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
   img: string;
-  location: string;
+  date: string;
+  dueDate: string;
   description: string;
 
   constructor() { }
 
   ngOnInit() {
     this.img = '../assets/innovation-logo.png';
-    this.location = 'at Yale';
+    this.date = 'April 23-27, 2018';
+    this.dueDate = 'Jan. 5';
     this.description = 'Empowering Substance Use Researchers to Translate their Innovations from the Lab to the World';
   }
 
