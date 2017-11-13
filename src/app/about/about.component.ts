1d63975cb94b0898ba8ef65c01a62caa5c14a0fc
zachnewburgh <zach.newburgh@gmail.com>
2017-11-12 21:35:59 -0500

    refactor(about): Change about text and formatting to include bullets

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,19 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   title: string;
   description: string;
+  info: any;
 
   constructor() { }
 
   ngOnInit() {
     this.title = 'THE PROGRAM';
     this.description = `
-      Innovation to Impact is a NIDA-funded education program in product development and entrepreneurship hosted at Yale University. We run
-      a free 5-day program for students, trainees (e.g., postdoctoral fellows) and faculty who are curious about how business principles
-      might help increase the reach of their research innovations. We recruit substance abuse researchers focused on basic science,
-      epidemiology, prevention, treatment and policy. We are interested in innovations that can impact any aspect of the substance use field
-      (e.g., pharmaceuticals, diagnostics, biomedical devices, app development, behavioral interventions). We are also interested in
-      scientists who may not have an innovation yet, but would like to be prepared to act when one emerges from their work.
+      Twenty million people are struggling with substance use disorders and the yearly economic impact in the US is $193B.  Innovations that
+      could help substance users are not reaching the public fast enough.  To address these challenges, Innovation to Impact at Yale was
+      developed as a national program to provide education, mentorship and seed funding to innovators.  The program is led by a team of
+      top-tier educators and entrepreneurs with the overriding goal to advance ideas to IMPACT the world.
     `
+
+    this.info = {
+      WHAT: `
+        A five-day program in New Haven, CT focused on teaching participants diverse skills through hands-on-learning activities with
+        guidance from dynamic leaders.  The program culminates in a Certificate in Entrepreneurship.`,
+      WHEN: `April 23-27, 2018.`,
+      COST: `Costs for travel, food, and accommodations will be covered for applicants who are selected for this prestigious program.`,
+      WHO: `Curious and committed innovators who are interested in both exploring what it takes to bring a product to market and
+            applying creative solutions to combat substance abuse through the use of pharmaceuticals, diagnostics, biomedical devices,
+            app development, behavioral interventions, and/or public policy.  Preference provided to researchers who have received NIDA
+            funding.`
+    }
   }
 
+  get infoHeadings(): string[] {
+    return Object.keys(this.info);
+  }
 }
